c51e583eb77017003af084e0064cee9a48d56d89
jooyg3-DEV <jooyg3@gmail.com>
2026-03-29 17:22:11 +0900

    Delete adapters directory

--- adapters/generic_playwright.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from adapters.portal_adapters import GenericDetailPlaywrightAdapter
-
-
-class GenericPlaywrightAdapter(GenericDetailPlaywrightAdapter):
-    pass
